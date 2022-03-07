e641c2dc7cd7a38fcf85d2349e038e6156963bb6
susannejung <95688233+susannejung@users.noreply.github.com>
2022-03-07 17:38:07 +0100

    Initial commit

---
+++ src/FilEksempler/Container.java
@@ -0,0 +1,6 @@
+package FilEksempler;
+
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ src/FilEksempler/Iterator.java
@@ -0,0 +1,6 @@
+package FilEksempler;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
